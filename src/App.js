31943b8b3e459a415fa130b30f550ef43ca4fcc9
Kaura Arja <s1900556@edu.bc.fi>
2019-10-13 15:04:54 +0300

    this is counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Counter from './Counter/Counter.js';
+import CounterHook from './CounterHook/CounterHook';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    return (
+    
+      <div>
+       {/* <Counter />*/}
+       <CounterHook />
+      </div>
+    )
+  }
+
+  
 
 export default App;
---
+++ src/Counter/Counter.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import './Counter.css';
+
+class Counter extends Component {
+    color = "";
+    state = {
+        number: 0,
+        background:'grey'
+    };
+    
+    backgroundHandler = (counter) => {
+        console.log(this.state.number);
+        return counter === 0 ? this.color = "grey"
+            :  counter%10 === 0 ? this.color = "goldenrod"
+            :  counter%2 === 0 ? this.color = "red"
+            :  this.color = "blue"
+    }
+
+    addHandler = () => {
+        console.log(this.state);
+        this.setState ( {number: this.state.number+1, background: this.backgroundHandler(this.state.number+1)} );
+        
+    }
+    
+    removeHandler = () => {
+        console.log("color: " + this.state.background);
+        console.log(this.state);
+        if (this.state.number > 0) {
+            this.setState({number: this.state.number-1, background: this.backgroundHandler(this.state.number-1)});
+        
+    }
+}
+
+    resetHandler = () => {
+        this.setState ({number: 0, background: 'grey'});
+    }
+
+    render () {
+        
+        return (
+            <div className='counter'>
+                <div className='circle'style={{backgroundColor: this.state.background}}>
+                    {this.state.number}
+                </div>
+                <div className='buttons'>
+                    <button onClick={this.addHandler}>Add one</button>
+                    <button onClick={this.removeHandler}>Remove one</button>
+                    <button onClick={this.resetHandler}>Reset</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Counter;
